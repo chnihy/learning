44bc9fd8d2e336515528944416d5494e63f4b84c
chnihy <69132469+chnihy@users.noreply.github.com>
2022-06-14 16:30:59 -0500

    Delete C++ directory

--- C++/forLoops/forLoops.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-
-int main(){
-	for (int i=2; i<=10; i++){
-		cout << i << endl;
-	}
-}
--- C++/namespaces/namespaces.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std; // not suggested for robust programs
-
-int main(){
-	string a = "four"; //if 'using namespace std'
-	std::string b = "five"; // if not 'using namespace std' 
-}
-
