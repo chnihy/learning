e2d21ef546024f764d6a85c2230b448ad7958476
chnihy <chnihy@gmail.com>
2022-12-21 10:39:33 -0600

    added Dictionary.cs to C#

---
+++ C#/dictionary/Dictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Declare a new Dictionary from System.Collections.Generic
+Dictionary<string, string> d = new Dictionary<string, string>();
+
+// Add kv pairs using the .Add() method
+d.Add("A", "0");
+Console.WriteLine(d["A"]);
+
+// Using a forloop and string indexes to generate kv pairs
+String myString = "BCDEFGHIJK";
+for(int x = 1; x < myString.Length + 1; x++){
+	string k = myString[x].ToString();
+	d.Add(k, x.ToString());
+}
+
+// printing kv pairs with a foreach loop
+foreach(KeyValuePair<string, string> kvp in d){
+	Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+}
